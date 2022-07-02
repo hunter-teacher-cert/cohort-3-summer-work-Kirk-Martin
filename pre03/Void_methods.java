fe7c859b83d0c5ee5fc89ad59f8a5148324e38ff
KIRK MARTIN <108408820+Kirk-Martin@users.noreply.github.com>
2022-07-02 03:53:12 +0000

    added void method program exercise

---
+++ pre03/Void_methods.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Hello {
+
+    public static void printHello(){
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args){
+        String s;
+        printHello();
+    }
+	
+}
