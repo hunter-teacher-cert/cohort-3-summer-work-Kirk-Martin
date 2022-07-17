8f413dfc759a50cf0f50c75b66283f9cc21a079a
KIRK MARTIN <108408820+Kirk-Martin@users.noreply.github.com>
2022-07-17 06:44:20 +0000

    Completing some data structures work.

---
+++ methods/PairProgramming.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class PairProgramming {
+
+ls
+public static void main (String[]args)
+
+
+ // { int []data= new int[] {0,1,2,3};
+    
+for (int i=0; i<size; i++)
+
+    System.out.println( barGraph (i)); 
+    
+   }
+    
+  }
+public static String barGraph(int i)
+  {if ( i==0){
+      
+     return "";
+   }
+
+      if (i==1) {
+         
+        return "="
+      }
+   
+      else  { 
+        return barGraph (i-1) + "=");
+            }  
+        
+         
+    
+
+
+  
+  }
+  
+
+
+
+  
+/**public static String barGraphify(int[]a)
+
+  {
+
+  String retStr= "";
+  for (int row=0;<....;row++){
+
+
+    ...
+    retStr+="\n";
+  }
+  return etStr;
+  }
+  }
+S**/
+                                 
+
+
+  
+}
