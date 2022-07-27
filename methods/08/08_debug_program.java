7aff5bf14a3e966fb106d0528b1fef548bb4a000
KIRK MARTIN <108408820+Kirk-Martin@users.noreply.github.com>
2022-07-27 03:51:07 +0000

    added lesson 8. Debugging method.

---
+++ methods/08/08_debug_program.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class DiceSimulation
+{
+  public int rollADie()
+  {
+    Random r = new Random();
+    int valueOfDie = r.nextInt(6)+1;
+
+    return valueOfDie;
+  }
+
+  public int[] computeDiceSum(int[] values)
+  {
+    int diceSums = 0;
+    for (int i = 0; i < values.length; i++)
+    {
+      diceSums = diceSums + values[i];
+    }
+    return diceSums;
+  }
+
+  public int[] generateDiceRolls(int simulations)
+  {
+    int[] diceRolls = new int[simulations];
+    for (int i = 0; i < diceRolls.length; i++)
+    {
+      diceRolls[i] = RollADie();
+    }
+    return diceRolls;
+  }
+  
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+    System.out.print("How many dice you would like to roll?" );
+    int diceNumber = input.nextInt();
+    int[] dice = new int[diceNumber];
+
+    for (int i = 0; i < dice.length; i++)
+    {
+      diceNumber[i] = rollADie();
+    }
+
+    System.out.print("How many times do you want to roll? ");
+    int simulations = input.nextInt();
+    int[] simRolls = new int[simulations];
+
+    simRolls = 
+//how would we print out what returns from rollADie?
+//System.out.println(rollADie()); Ben's guess
+
+//how would we print out what returns from computeDiceSum?
+
+
+//How would we print out what returns from generateDiceRolls?
+  }
+}
